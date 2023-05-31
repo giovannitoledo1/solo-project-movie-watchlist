3b706789af7170edb2776752e4195a0dced6f2a6
giovannitoledo1 <giovanni.toledo507@myci.csuci.edu>
2023-05-31 12:23:01 -0700

    adjustments

--- index.js
+++ index.js
@@ -8,43 +8,53 @@ const movie = {
     poster: [],
     plot: []
 }
-searchBtnElm.addEventListener('click', function(){
-    function renderContent(){
-        let html = ""
-        startExploringWrapperElm.style.marginTop = '100px'
-        for (let i =0; i < movie.title.length; i++){
-            html += `
-                <div class="movie-container">
-                    <div id="poster-container">
-                        <img id="poster" src="${movie.poster[i]}" />
-                    </div>
-                    <div id="movie-detials-container">
-                        <h3 id="title">${movie.title[i]}</h3>
-                        <p id"description">${movie.year[i]}</p>
-                        <p id="plot">${movie.plot[i]}</p>
-                    </div>
-                    <hr/>
-                </div>     
-            `
-        }
-        startExploringWrapperElm.innerHTML = html
-    }
 
+function renderContent(){
+    let html = ""
+    startExploringWrapperElm.style.marginTop = '50px'
+    for (let i =0; i < movie.title.length; i++){
+        html += `
+            <div class="movie-container">
+                <div id="poster-container">
+                    <img id="poster" src="${movie.poster[i]}" />
+                </div>
+                <div id="movie-detials-container">
+                    <h3 id="title">${movie.title[i]}</h3>
+                    <p id"description">${movie.year[i]}</p>
+                    <p id="plot">${movie.plot[i]}</p>
+                </div> 
+            </div>
+            <hr/>  
+        `
+    }
+    startExploringWrapperElm.innerHTML = html
+}
+
+function noMovieFound(){
+    startExploringWrapperElm.innerHTML = `
+        <h3 id="title">Unable to find what you're looking for.<br>
+            Please try aother search.
+        </h3> 
+    `
+}
+
+searchBtnElm.addEventListener('click', function(){
+    if(searchInputElm.value === ""){
+        noMovieFound()
+    } else {
     fetch(`http://www.omdbapi.com/?t=${searchInputElm.value}&apikey=f895ed9b`)
         .then(res => res.json())
         .then(data=> {
-            console.log("Printing the data.search: " + data.Plot)
-            movie.title.unshift(data.Title)
-            movie.year.unshift(data.Year)
-            movie.poster.unshift(data.Poster)
-            movie.plot.unshift(data.Plot)
-            renderContent();
-        // console.log(title, length)
-        // console.log(movie.plot)
+                movie.title.unshift(data.Title)
+                movie.year.unshift(data.Year)
+                movie.poster.unshift(data.Poster)
+                movie.plot.unshift(data.Plot)
+                renderContent();            
         })
         .catch(err => {
-            console.log(err)
+            // noMovieFound()
         })
+    }
 })
 
 
